8f0b61392d6f9f3e96a1a51d17be9b1be9f24f07
sachinhd517 <sachinhd517@gmail.com>
2026-04-01 23:04:28 +0530

    DefaultConnection add with register and add services to the container

--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -1,10 +1,16 @@
 
+using Mango.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 
+});
 
 
 builder.Services.AddControllers();
