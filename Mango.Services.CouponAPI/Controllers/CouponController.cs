86d32a4c16a872df0f96e8c38bc460f93f5a5c64
sachinhd517 <sachinhd517@gmail.com>
2026-04-02 00:27:33 +0530

    create Object of IEnumerable of list and single record using Object

---
+++ Mango.Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,50 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public CouponController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public object? Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList(); // select * from coupon
+                return objList;
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+            return null;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object? Get(int id)
+        {
+            try
+            {
+                Coupon objList = _db.Coupons.First(u => u.CouponId == id); // select * from coupon where id = id
+                return objList;
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+            return null;
+        }
+    }
+}
