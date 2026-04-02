fbb4384d774a03da80616276fab6f0c5a9b3f428
sachinhd517 <sachinhd517@gmail.com>
2026-04-02 23:22:03 +0530

    Update the Coupon code using HttpPut method

--- Mango.Services.CouponAPI/Controllers/CouponController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponController.cs
@@ -105,5 +105,25 @@ namespace Mango.Services.CouponAPI.Controllers
             }
             return _response;
         }
+
+
+        [HttpPut]
+        public ResponseDto? Put([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Update(obj);
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                //ex.Message.ToString();
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
     }
 }
