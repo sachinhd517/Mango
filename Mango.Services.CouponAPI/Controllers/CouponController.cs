d4ca7903a212d5ee664df2e34ef1524f3f6ea1bb
sachinhd517 <sachinhd517@gmail.com>
2026-04-02 20:37:58 +0530

    create method GetByCode httpGet method

--- Mango.Services.CouponAPI/Controllers/CouponController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponController.cs
@@ -56,5 +56,26 @@ namespace Mango.Services.CouponAPI.Controllers
             }
             return _response;
         }
+
+
+
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public ResponseDto? GetByCode(string code)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.FirstOrDefault(u => u.CouponCode.ToLower() == code.ToLower()); // select * from coupon where id = id
+                _response.Result = _mapper.Map<CouponDto>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                //ex.Message.ToString();
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
     }
 }
