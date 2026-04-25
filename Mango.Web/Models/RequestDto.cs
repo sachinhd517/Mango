4868488d21d175d44f7eb319571e3cd4c925dd7f
sachinhd517 <sachinhd517@gmail.com>
2026-04-25 17:52:29 +0530

    Create file RequestDto.cs file

---
+++ Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Principal;
+
+namespace Mango.Web.Models
+{
+    public class RequestDto
+    {
+        public string ApiType { get; set; } = "GET";
+        public string url { get; set; }
+        public object Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
