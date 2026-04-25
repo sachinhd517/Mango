790a2c24e015b4f787d3d2bdfc170d88eabe79c4
sachinhd517 <sachinhd517@gmail.com>
2026-04-25 17:59:17 +0530

    import ApiType to RequestDto fileds

--- Mango.Web/Models/RequestDto.cs
+++ Mango.Web/Models/RequestDto.cs
@@ -1,10 +1,11 @@
 ﻿using System.Security.Principal;
+using static Mango.Web.Utility.SD;
 
 namespace Mango.Web.Models
 {
     public class RequestDto
     {
-        public string ApiType { get; set; } = "GET";
+        public ApiType ApiType { get; set; } = ApiType.GET;
         public string url { get; set; }
         public object Data { get; set; }
         public string AccessToken { get; set; }
