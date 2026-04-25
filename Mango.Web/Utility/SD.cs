2e1fa99b7e59593799b254ef941082380007f89e
sachinhd517 <sachinhd517@gmail.com>
2026-04-25 17:58:37 +0530

    Create enum for get, post, put, delete

---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Mango.Web.Utility
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+
+    }
+}
