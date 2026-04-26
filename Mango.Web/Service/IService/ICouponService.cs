069f134666d7d2ad43b807eba1556f42e3d8907b
sachinhd517 <sachinhd517@gmail.com>
2026-04-26 13:40:24 +0530

    create Coupon Model and create ICouponService interface and wrote the Methods GetCouponAsync, GetAllCouponAsync, GetCouponAsync, GetCreateCouponAsync, GetUpdateCouponAsync, GetDeleteCouponAsync

---
+++ Mango.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto?> GetCouponAsync(string couponCode);
+        Task<ResponseDto?> GetAllCouponAsync();
+        Task<ResponseDto?> GetCouponAsync(int id);
+        Task<ResponseDto?> GetCreateCouponAsync(CouponDto couponCode);
+        Task<ResponseDto?> GetUpdateCouponAsync(CouponDto couponDto);
+        Task<ResponseDto?> GetDeleteCouponAsync(int id);
+        
+    }
+}
